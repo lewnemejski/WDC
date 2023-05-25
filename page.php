ff8153cf4a73d3d49dfc26c5710697273d816236
lewnemejski <100286062+lewnemejski@users.noreply.github.com>
2023-05-25 15:34:17 +0200

    Add files via upload

---
+++ page.php
@@ -0,0 +1,83 @@
+<?php
+echo '<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+
+	<meta charset="utf-8"/>
+	<title>Jaśminowy ogródek</title>
+	<meta name="description" content="Strona poświęcona kwiatom i motylom."/>
+	<meta name="keywords" content=""/>
+	<meta name="author" content="s189477"/>
+	
+	<meta http-equiv="X-Ua-Compatible" content="IE=edge"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/galery_style.css"/>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet"> 
+	<link href="fontAwesome/css/all.css" rel="stylesheet">
+	
+	<script src="js/script.js" defer></script>
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+	<script>
+			document.addEventListener("DOMContentLoaded", function() {
+			  tryb(); navi();
+			});
+		</script>
+	<noscript>
+		
+			<style>
+				.noscript{
+					display: block;
+					height: 130px;
+				}
+				#sun{
+					display: none;
+				}
+
+			</style>
+			
+		</noscript>
+	
+</head>
+
+<body>
+		
+	<div id="container" class="light">	
+		<div id="header">
+			<header>
+			
+				<div class="logo">
+					<img id="logo_motyl" src="img/motyl.png" alt="Logo strony" class="responsive"/> 
+					<br/>Jaśminowy ogródek
+				</div>
+			<nav>
+				<div id="nav">
+					<ol>
+						<li><a href="index.php"><i class="fas fa-home"></i></a></li>
+						<li><a href="galery.php">Galeria</a>
+								<ul>
+									<li><a href="galery.php">Serwisu</a></li>
+									<li><a href="usergalery.php">Użytkowników</a></li>
+								</ul>
+							</li>
+						<li><a href="garden.php">Ogródek</a></li>
+						<li><a href="contact.php">Kontakt</a></li>
+						<li><a href="login.php" role="button"><i class="fas fa-user"></i></a></li>
+						<li><a href="#" role="button" onClick="dark()"><i id="sun" class="fas fa-moon"></i></a></li>
+					</ol>
+				</div>
+			</nav>	
+				<div class="noscript">
+					<i class="fas fa-exclamation-triangle"></i><p>Strona wymaga do poprawnego działania włączonego JS.	Włącz w przeglądarce JS oraz/albo wyłącz dodatki blokujące JS, a następnie odśwież witrynę.
+					<br/> 
+					<span>Dziękujemy za zrozumienie i życzymy miłego korzystania z serwisu.</span><br/>
+				</div>
+				
+			</header>
+		</div>
+		<div id="content" style="text-align:center;">'
+?>
